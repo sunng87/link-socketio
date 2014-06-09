2ea6380421df9e91e0ff7d52601275b27b72d2b4
Sun Ning <nsun@avoscloud.com>
2014-06-09 16:05:52 +0800

    added lein-release config

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.corundumstudio.socketio/netty-socketio "1.6.5"]
                  [link "0.6.9"]]
-  :profiles {:example {:source-paths ["example"]}})
+  :profiles {:example {:source-paths ["example"]}}
+  :lein-release {:scm :git
+                 :deploy-via :shell
+                 :shell ["lein" "deploy" "clojars"]})
