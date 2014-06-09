461730ca0b89cdad4011d8991c22f551f19ef6e4
Sun Ning <nsun@avoscloud.com>
2014-06-09 16:40:24 +0800

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject link/link-socketio "0.1.0"
+(defproject link/link-socketio "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
