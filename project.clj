b00869c1debd3604094372ad134871d85eabe419
Sun Ning <sunng@about.me>
2014-07-07 15:39:49 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
 (defproject link/link-socketio "0.2.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "SocketIO server in link verbs."
+  :url "http://github.com/sunng87/link-socketio"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.corundumstudio.socketio/netty-socketio "1.7.0"]
-                 [link "0.6.9"]]
+                 [com.corundumstudio.socketio/netty-socketio "1.7.1"]
+                 [link "0.6.13"]]
   :profiles {:example {:source-paths ["example"]
                        :dependencies [[log4j "1.2.17"]
                                       [org.slf4j/slf4j-log4j12 "1.7.6"]]}}
-  :lein-release {:scm :git
-                 :deploy-via :shell
-                 :shell ["lein" "deploy" "clojars"]})
+  :deploy-repositories {"releases" :clojars})
